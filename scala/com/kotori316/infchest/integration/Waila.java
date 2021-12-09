d9fed387d227141970fdf0835f45f355a8daf811
Kotori316 <Kotori316@gmail.com>
2021-12-09 21:53:45 +0900

    Update tooltip mods.

--- scala/com/kotori316/infchest/integration/Waila.java
+++ scala/com/kotori316/infchest/integration/Waila.java
@@ -1,20 +1,20 @@
 package com.kotori316.infchest.integration;
-/*
+
 import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.TooltipPosition;
 import mcp.mobius.waila.api.WailaPlugin;
 
+import com.kotori316.infchest.InfChest;
 import com.kotori316.infchest.tiles.TileInfChest;
 
-@WailaPlugin
+@WailaPlugin(id = InfChest.modID + ":wthit_plugin")
 public class Waila implements IWailaPlugin {
 
     @Override
     public void register(IRegistrar registrar) {
         WailaProvider provider = new WailaProvider();
-        registrar.registerComponentProvider(provider, TooltipPosition.BODY, TileInfChest.class);
-        registrar.registerBlockDataProvider(provider, TileInfChest.class);
+        registrar.addComponent(provider, TooltipPosition.BODY, TileInfChest.class);
+        registrar.addBlockData(provider, TileInfChest.class);
     }
 }
-*/
--- scala/com/kotori316/infchest/integration/WailaProvider.java
+++ scala/com/kotori316/infchest/integration/WailaProvider.java
@@ -1,36 +1,35 @@
 package com.kotori316.infchest.integration;
-/*
-import java.math.BigInteger;
-import java.util.List;
 
-import mcp.mobius.waila.api.IComponentProvider;
-import mcp.mobius.waila.api.IDataAccessor;
+import java.math.BigInteger;
+
+import mcp.mobius.waila.api.IBlockAccessor;
+import mcp.mobius.waila.api.IBlockComponentProvider;
 import mcp.mobius.waila.api.IPluginConfig;
 import mcp.mobius.waila.api.IServerDataProvider;
-import net.minecraft.entity.player.ServerPlayer;
-import net.minecraft.item.ItemStack;
+import mcp.mobius.waila.api.ITooltip;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.tileentity.BlockEntity;
-import net.minecraft.util.text.Component;
-import net.minecraft.util.text.TextComponent;
-import net.minecraft.world.Level;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 import com.kotori316.infchest.tiles.TileInfChest;
 
-public class WailaProvider implements IServerDataProvider<BlockEntity>, IComponentProvider {
+public class WailaProvider implements IServerDataProvider<BlockEntity>, IBlockComponentProvider {
     private static final String NBT_ITEM = "waila_item";
     private static final String NBT_COUNT = "waila_count";
 
     @Override
-    public void appendBody(List<Component> tooltip, IDataAccessor accessor, IPluginConfig config) {
+    public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
         BlockEntity t = accessor.getBlockEntity();
         if (t instanceof TileInfChest) {
             CompoundTag data = accessor.getServerData();
             ItemStack stack;
             if (data.contains(NBT_ITEM)) {
-                stack = ItemStack.read(data.getCompound(NBT_ITEM));
+                stack = ItemStack.of(data.getCompound(NBT_ITEM));
             } else {
-                stack = ItemStack.read(data.getCompound(TileInfChest.NBT_ITEM));
+                stack = ItemStack.of(data.getCompound(TileInfChest.NBT_ITEM));
             }
             if (!stack.isEmpty()) {
                 BigInteger integer;
@@ -48,12 +47,10 @@ public class WailaProvider implements IServerDataProvider<BlockEntity>, ICompone
 
     @Override
     public void appendServerData(CompoundTag tag, ServerPlayer serverPlayer, Level world, BlockEntity te) {
-        if (te instanceof TileInfChest) {
-            TileInfChest chest = (TileInfChest) te;
+        if (te instanceof TileInfChest chest) {
             tag.put(NBT_ITEM, chest.getStack(1).serializeNBT());
             tag.putByteArray(NBT_COUNT, chest.itemCount().toByteArray());
         }
     }
 
 }
-*/
