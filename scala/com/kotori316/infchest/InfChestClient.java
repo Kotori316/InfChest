74a36daeb2b876af482de19490bdbe03ca48c92f
Kotori316 <Kotori316@gmail.com>
2022-06-11 23:23:51 +0900

    Plugin for Jade

--- scala/com/kotori316/infchest/InfChestClient.java
+++ scala/com/kotori316/infchest/InfChestClient.java
@@ -1,7 +1,7 @@
 package com.kotori316.infchest;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.minecraft.client.gui.screens.MenuScreens;
 
 import com.kotori316.infchest.guis.GuiInfChest;
 import com.kotori316.infchest.packets.PacketHandler;
@@ -10,6 +10,6 @@ public class InfChestClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         PacketHandler.Client.initClient();
-        ScreenRegistry.register(InfChest.Register.INF_CHEST_CONTAINER_TYPE, GuiInfChest::new);
+        MenuScreens.register(InfChest.Register.INF_CHEST_CONTAINER_TYPE, GuiInfChest::new);
     }
 }
--- scala/com/kotori316/infchest/integration/InfChestWthitProvider.java
+++ scala/com/kotori316/infchest/integration/InfChestWthitProvider.java
@@ -9,7 +9,6 @@ import mcp.mobius.waila.api.IServerAccessor;
 import mcp.mobius.waila.api.IServerDataProvider;
 import mcp.mobius.waila.api.ITooltip;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -17,40 +16,28 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import com.kotori316.infchest.tiles.TileInfChest;
 
 public class InfChestWthitProvider implements IServerDataProvider<BlockEntity>, IBlockComponentProvider {
-    private static final String NBT_ITEM = "waila_item";
-    private static final String NBT_COUNT = "waila_count";
-    private static final String NBT_OUTPUT = "waila_output";
+    private static final String NBT_ITEM = "wthit_item";
+    private static final String NBT_COUNT = "wthit_count";
 
     @Override
     public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
-        BlockEntity t = accessor.getBlockEntity();
-        if (t instanceof TileInfChest) {
-            final CompoundTag data = accessor.getServerData();
-            final ItemStack stack;
+        if (accessor.getBlockEntity() instanceof TileInfChest) {
+            CompoundTag data = accessor.getServerData();
+            ItemStack stack;
             if (data.contains(NBT_ITEM)) {
                 stack = ItemStack.of(data.getCompound(NBT_ITEM));
             } else {
                 stack = ItemStack.of(data.getCompound(TileInfChest.NBT_ITEM));
             }
-            final int outputItem;
-            if (data.contains(NBT_OUTPUT)) {
-                outputItem = ItemStack.of(data.getCompound(NBT_OUTPUT)).getCount();
-            } else {
-                outputItem = data.getList("Items", Tag.TAG_COMPOUND).stream()
-                    .map(CompoundTag.class::cast)
-                    .filter(e -> e.getByte("Slot") == 1) // Find the output slot.
-                    .mapToInt(e -> e.getInt("Count"))
-                    .findFirst().orElse(0);
-            }
             if (!stack.isEmpty()) {
-                final BigInteger integer;
+                BigInteger integer;
                 if (data.contains(NBT_COUNT))
-                    integer = new BigInteger(data.getByteArray(NBT_COUNT)).add(BigInteger.valueOf(outputItem));
+                    integer = new BigInteger(data.getByteArray(NBT_COUNT));
                 else if (data.contains(TileInfChest.NBT_COUNT))
-                    integer = new BigInteger(data.getString(TileInfChest.NBT_COUNT)).add(BigInteger.valueOf(outputItem));
+                    integer = new BigInteger(data.getString(TileInfChest.NBT_COUNT)).subtract(BigInteger.valueOf(stack.getMaxStackSize()));
                 else
                     integer = BigInteger.ZERO;
-                tooltip.addLine(stack.getHoverName());
+                tooltip.addLine(stack.getDisplayName());
                 tooltip.addLine(Component.literal(integer.toString()));
             }
         }
@@ -58,11 +45,9 @@ public class InfChestWthitProvider implements IServerDataProvider<BlockEntity>, 
 
     @Override
     public void appendServerData(CompoundTag tag, IServerAccessor<BlockEntity> accessor, IPluginConfig config) {
-        var te = accessor.getTarget();
-        if (te instanceof TileInfChest chest) {
-            tag.put(NBT_ITEM, chest.getStackWithAmount(1).save(new CompoundTag())); // Holding item
-            tag.putByteArray(NBT_COUNT, chest.itemCount().toByteArray()); // Item count
-            tag.put(NBT_OUTPUT, chest.getItem(1).save(new CompoundTag())); // Item in output slot
+        if (accessor.getTarget() instanceof TileInfChest chest) {
+            tag.put(NBT_ITEM, chest.getStackWithAmount(1).save(new CompoundTag()));
+            tag.putByteArray(NBT_COUNT, chest.itemCount().toByteArray());
         }
     }
 
