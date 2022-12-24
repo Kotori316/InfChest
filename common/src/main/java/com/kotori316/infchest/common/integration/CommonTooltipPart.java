87844f50b954387c000671f902709ce70a0b98db
Kotori316 <Kotori316@gmail.com>
2022-12-24 20:57:24 +0900

    Extract common parts of tooltip integration

---
+++ common/src/main/java/com/kotori316/infchest/common/integration/CommonTooltipPart.java
@@ -0,0 +1,44 @@
+package com.kotori316.infchest.common.integration;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+import com.kotori316.infchest.common.tiles.TileInfChest;
+
+public final class CommonTooltipPart {
+    private static final String NBT_KEY_ITEM = "tooltip_item";
+    private static final String NBT_KEY_COUNT = "tooltip_count";
+
+    public static List<Component> getTooltipBodyParts(CompoundTag data) {
+        ItemStack stack;
+        if (data.contains(NBT_KEY_ITEM)) {
+            stack = ItemStack.of(data.getCompound(NBT_KEY_ITEM));
+        } else {
+            stack = ItemStack.of(data.getCompound(TileInfChest.NBT_ITEM));
+        }
+        if (!stack.isEmpty()) {
+            BigInteger integer;
+            if (data.contains(NBT_KEY_COUNT))
+                integer = new BigInteger(data.getByteArray(NBT_KEY_COUNT));
+            else if (data.contains(TileInfChest.NBT_COUNT))
+                integer = new BigInteger(data.getString(TileInfChest.NBT_COUNT)).subtract(BigInteger.valueOf(stack.getMaxStackSize()));
+            else
+                integer = BigInteger.ZERO;
+            return List.of(stack.getDisplayName(), Component.literal(integer.toString()));
+        } else {
+            return List.of();
+        }
+    }
+
+    public static void addTileData(CompoundTag destination, BlockEntity maybeChest) {
+        if (maybeChest instanceof TileInfChest chest) {
+            destination.put(NBT_KEY_ITEM, chest.getItem(1).save(new CompoundTag()));
+            destination.putByteArray(NBT_KEY_COUNT, chest.itemCount().toByteArray());
+        }
+    }
+}
--- forge/src/main/java/com/kotori316/infchest/forge/integration/InfChestJadeProvider.java
+++ forge/src/main/java/com/kotori316/infchest/forge/integration/InfChestJadeProvider.java
@@ -1,12 +1,8 @@
 package com.kotori316.infchest.forge.integration;
 
-import java.math.BigInteger;
-
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import snownee.jade.api.BlockAccessor;
@@ -17,43 +13,22 @@ import snownee.jade.api.TooltipPosition;
 import snownee.jade.api.config.IPluginConfig;
 
 import com.kotori316.infchest.common.InfChest;
+import com.kotori316.infchest.common.integration.CommonTooltipPart;
 import com.kotori316.infchest.common.tiles.TileInfChest;
 
 public class InfChestJadeProvider implements IServerDataProvider<BlockEntity>, IBlockComponentProvider {
-    private static final String NBT_ITEM = "jade_item";
-    private static final String NBT_COUNT = "jade_count";
 
     @Override
     public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
-        BlockEntity t = accessor.getBlockEntity();
-        if (t instanceof TileInfChest) {
-            CompoundTag data = accessor.getServerData();
-            ItemStack stack;
-            if (data.contains(NBT_ITEM)) {
-                stack = ItemStack.of(data.getCompound(NBT_ITEM));
-            } else {
-                stack = ItemStack.of(data.getCompound(TileInfChest.NBT_ITEM));
-            }
-            if (!stack.isEmpty()) {
-                BigInteger integer;
-                if (data.contains(NBT_COUNT))
-                    integer = new BigInteger(data.getByteArray(NBT_COUNT));
-                else if (data.contains(TileInfChest.NBT_COUNT))
-                    integer = new BigInteger(data.getString(TileInfChest.NBT_COUNT)).subtract(BigInteger.valueOf(stack.getMaxStackSize()));
-                else
-                    integer = BigInteger.ZERO;
-                tooltip.add(stack.getDisplayName());
-                tooltip.add(Component.literal(integer.toString()));
-            }
+        if (accessor.getBlockEntity() instanceof TileInfChest) {
+            CommonTooltipPart.getTooltipBodyParts(accessor.getServerData())
+                .forEach(tooltip::add);
         }
     }
 
     @Override
     public void appendServerData(CompoundTag tag, ServerPlayer serverPlayer, Level world, BlockEntity te, boolean b) {
-        if (te instanceof TileInfChest chest) {
-            tag.put(NBT_ITEM, chest.getStack(1).serializeNBT());
-            tag.putByteArray(NBT_COUNT, chest.itemCount().toByteArray());
-        }
+        CommonTooltipPart.addTileData(tag, te);
     }
 
     @Override
