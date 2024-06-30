4883199a746dcb1da077657ee277ae0fdd7065a2
Kotori316 <Kotori316@gmail.com>
2024-06-30 19:54:07 +0900

    Set 1.21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,7 +32,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
 }
 
-rootProject.name = "InfChest-1.20"
+rootProject.name = "InfChest-1.21"
 includeBuild("build-logic")
 include("common")
 if (!(System.getenv("DISABLE_FORGE") ?: "false").toBoolean()) {
