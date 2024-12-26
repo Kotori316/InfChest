2c7452663a74f4912ffb9f12eae05f327a966b2c
Kotori316 <Kotori316@gmail.com>
2024-12-26 13:55:41 +0900

    Update data generator

--- neoforge/src/genData/java/com/kotori316/infchest/data/neoforge/StateAndModelProvider.java
+++ neoforge/src/genData/java/com/kotori316/infchest/data/neoforge/StateAndModelProvider.java
@@ -1,57 +1,25 @@
 package com.kotori316.infchest.data.neoforge;
 
 import com.kotori316.infchest.common.InfChest;
-import com.kotori316.infchest.common.blocks.BlockDeque;
-import com.kotori316.infchest.common.blocks.BlockInfChest;
-import net.minecraft.client.data.models.model.ItemModelUtils;
-import net.minecraft.client.data.models.model.TextureMapping;
-import net.minecraft.client.renderer.item.ClientItem;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.data.CachedOutput;
-import net.minecraft.data.DataProvider;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.client.data.models.model.TexturedModel;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
-import net.neoforged.neoforge.client.model.generators.ModelFile;
-import net.neoforged.neoforge.client.model.generators.ModelProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
+public final class StateAndModelProvider extends ModelProvider {
 
-public final class StateAndModelProvider extends BlockStateProvider {
-    private final PackOutput.PathProvider itemInfoPathProvider;
-    private final Map<ResourceLocation, ClientItem> clientItemMap = new HashMap<>();
-
-    public StateAndModelProvider(PackOutput output, ExistingFileHelper exFileHelper) {
-        super(output, InfChest.modID, exFileHelper);
-        this.itemInfoPathProvider = output.createPathProvider(PackOutput.Target.RESOURCE_PACK, "items");
+    public StateAndModelProvider(PackOutput output) {
+        super(output, InfChest.modID);
     }
 
     @Override
-    protected void registerStatesAndModels() {
-        this.simpleBlockWithItem(InfChest.accessor.CHEST(), models().cubeAll(BlockInfChest.name, modLoc(ModelProvider.BLOCK_FOLDER + "/" + "if")));
-        this.simpleBlockWithItem(InfChest.accessor.DEQUE(), models().cubeAll(BlockDeque.name, modLoc(ModelProvider.BLOCK_FOLDER + "/" + BlockDeque.name)));
-    }
-
-    @Override
-    public void simpleBlockItem(Block block, ModelFile model) {
-        super.simpleBlockItem(block, model);
-        var key = BuiltInRegistries.BLOCK.getKey(block);
-        var unbaked = ItemModelUtils.plainModel(TextureMapping.getItemTexture(block.asItem()));
-        clientItemMap.put(
-            key,
-            new ClientItem(unbaked, ClientItem.Properties.DEFAULT)
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        blockModels.blockStateOutput.accept(
+            BlockModelGenerators.createSimpleBlock(InfChest.accessor.CHEST(), TexturedModel.CUBE.updateTexture(t -> t.put(TextureSlot.ALL, ResourceLocation.fromNamespaceAndPath(InfChest.modID, "block/if"))).create(InfChest.accessor.CHEST(), blockModels.modelOutput))
         );
-    }
-
-    @Override
-    public CompletableFuture<?> run(CachedOutput cache) {
-        var parent = super.run(cache);
-        return parent.thenCompose(v ->
-            DataProvider.saveAll(cache, ClientItem.CODEC, itemInfoPathProvider, clientItemMap)
-        );
+        blockModels.createTrivialCube(InfChest.accessor.DEQUE());
     }
 }
