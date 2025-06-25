f96b4a6d0a3d419490f34127fc978c8a626990de
Kotori316 <Kotori316@gmail.com>
2025-06-25 23:15:41 +0900

    Improve render

--- common/src/main/java/com/kotori316/infchest/common/guis/GuiInfChest.java
+++ common/src/main/java/com/kotori316/infchest/common/guis/GuiInfChest.java
@@ -40,7 +40,6 @@ public class GuiInfChest extends AbstractContainerScreen<ContainerInfChest> {
 
     @Override
     public void render(GuiGraphics graphics, final int mouseX, final int mouseY, final float partialTicks) {
-        this.renderBackground(graphics, mouseX, mouseY, partialTicks);// background
         super.render(graphics, mouseX, mouseY, partialTicks);
         this.renderTooltip(graphics, mouseX, mouseY); // render tooltip
     }
