9fa04a875efb1a31b99594ddb871844340afad56
Kotori316 <Kotori316@gmail.com>
2026-01-25 22:17:51 +0900

    Fix test

--- fabric/src/test/java/com/kotori316/infchest/fabric/tiles/TileInfChestTest.java
+++ fabric/src/test/java/com/kotori316/infchest/fabric/tiles/TileInfChestTest.java
@@ -7,10 +7,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestAssertException;
 import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.item.enchantment.Enchantments;
 
 import java.util.Objects;
@@ -138,8 +136,9 @@ public class TileInfChestTest implements FabricGameTest {
         var pos = new BlockPos(0, 1, 0);
         helper.setBlock(pos, InfChest.accessor.CHEST());
 
-        var tile = (TileInfChestFabric) Objects.requireNonNull(helper.getBlockEntity(pos));
-        var enchantedBook = EnchantedBookItem.createForEnchantment(new EnchantmentInstance(helper.getLevel().registryAccess().registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.FORTUNE), 2));
+        TileInfChestFabric tile = helper.getBlockEntity(pos);
+        var enchantedBook = new ItemStack(Items.ENCHANTED_BOOK);
+        enchantedBook.enchant(helper.getLevel().registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE), 2);
         tile.setItem(0, enchantedBook);
 
         var stack = tile.getItem(1);
