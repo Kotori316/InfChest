d5231b3fba8bddcf9f9b41f9dcdbca983e1a769f
Kotori316 <Kotori316@gmail.com>
2026-04-18 22:41:25 +0900

    Run data

--- neoforge/src/genData/java/com/kotori316/infchest/data/neoforge/StateAndModelProvider.java
+++ neoforge/src/genData/java/com/kotori316/infchest/data/neoforge/StateAndModelProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
 import net.minecraft.client.data.models.model.TextureSlot;
 import net.minecraft.client.data.models.model.TexturedModel;
+import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
 
@@ -17,7 +18,7 @@ public final class StateAndModelProvider extends ModelProvider {
 
     @Override
     protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
-        var variant = BlockModelGenerators.plainVariant(TexturedModel.CUBE.updateTexture(t -> t.put(TextureSlot.ALL, Identifier.fromNamespaceAndPath(InfChest.modID, "block/if"))).create(InfChest.accessor.CHEST(), blockModels.modelOutput));
+        var variant = BlockModelGenerators.plainVariant(TexturedModel.CUBE.updateTexture(t -> t.put(TextureSlot.ALL, new Material(Identifier.fromNamespaceAndPath(InfChest.modID, "block/if")))).create(InfChest.accessor.CHEST(), blockModels.modelOutput));
         blockModels.blockStateOutput.accept(
             BlockModelGenerators.createSimpleBlock(InfChest.accessor.CHEST(), variant)
         );
